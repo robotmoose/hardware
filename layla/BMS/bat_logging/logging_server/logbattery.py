0a898bae1cfaf3dfb6844aed2defb2261efe59b4
Mike Moss <mrmoss@alaska.edu>
2017-03-04 12:15:15 -0900

    python...

--- layla/BMS/bat_logging/logging_server/logbattery.py
+++ layla/BMS/bat_logging/logging_server/logbattery.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2.7
+#!/usr/bin/env python2
 
 #import os; activate_this=os.path.join(os.path.dirname(os.path.realpath(__file__)), 'activate_this.py'); exec(compile(open(activate_this).read(), activate_this, 'exec'), dict(__file__=activate_this)); del os, activate_this
 
